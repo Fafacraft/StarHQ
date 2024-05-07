efa8529914e859c82f52dee9ec5c7a8e0cf76a35
Fafacraft <fafa24252425@yahoo.com>
2024-05-07 22:43:26 +0200

    most of the basics done

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Test;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FirstController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em){
+        $this->em = $em;
+    }
+
     #[Route('/first/{name}', name: 'app_first', defaults:['name' => null], methods:['GET', 'HEAD'])]
     public function index($name): Response
     {
@@ -17,5 +24,15 @@ class FirstController extends AbstractController
             'controller_name' => 'FirstController',
             'tests' => $tests,
         ]);
+    }
+
+    #[Route('/second', name: 'second')]
+    public function second(EntityManagerInterface $em): Response
+    {
+        $repository = $this->em->getRepository(Test::class);
+        $movies = $repository->findAll();
+
+        dd($movies);
+        return $this->render('index.html.twig');
     }
 }
