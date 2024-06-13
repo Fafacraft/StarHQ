7e68298d973ca5ef15c015fff3d957bc05ccabb3
FabienArthurYnov <fabien.arthur@ynov.com>
2024-06-13 11:44:34 +0200

    fix crash going to prod

--- src/Controller/AllShipsController.php
+++ src/Controller/AllShipsController.php
@@ -97,12 +97,47 @@ class AllShipsController extends AbstractController
             $ship_clear['imageLink'] = $imageLink;
 
             // change the color of the "role" part on the ship card, depending of each role
-            $ship_clear['role_color'] = getRoleColor($ship_clear['role']);
+            $ship_clear['role_color'] = $this->getRoleColor($ship_clear['role']);
             $ship_clear['with_button'] = false;
 
             array_push($ships_final, $ship_clear);
         }
 
         return $ships_final;
+    }
+
+    private function getRoleColor($role) {
+
+        $role_color = '';
+        switch ($role) {
+            case "combat":
+                $role_color = "#e23535"; // red
+                break;
+            case "exploration":
+                $role_color = "#2d89e1"; // blue
+                break;
+            case "competition":
+                $role_color = "#08b122"; // green
+                break;
+            case "industrial":
+                $role_color = "#f8d142"; // yellow
+                break;
+            case "multi":
+                $role_color = "#3aebc8"; // cyan
+                break;
+            case "ground":
+                $role_color = "#a96f4c"; // brown
+                break;
+            case "support":
+                $role_color = "#f2aae4"; // pink
+                break;
+            case "transport":
+                $role_color = "#f68948"; // orange
+                break;
+            default:
+                $role_color = "";
+        }
+    
+        return $role_color;
     }
 }
--- src/Controller/ShipPageController.php
+++ src/Controller/ShipPageController.php
@@ -41,7 +41,7 @@ class ShipPageController extends AbstractController
         $imageLink = $this->em->getRepository(ShipImage::class)->findLinkByName($name);
 
         // get role color
-        $role_color = getRoleColor($ship->getRole());
+        $role_color = $this->getRoleColor($ship->getRole());
 
         // get nb of ship owned for the logged user
         $email = $this->getUser()->getUserIdentifier();
@@ -56,5 +56,40 @@ class ShipPageController extends AbstractController
             "role_color" => $role_color,
             "nb_ship" => $nb_ship,
         ]);
+    }
+
+    private function getRoleColor($role) {
+
+        $role_color = '';
+        switch ($role) {
+            case "combat":
+                $role_color = "#e23535"; // red
+                break;
+            case "exploration":
+                $role_color = "#2d89e1"; // blue
+                break;
+            case "competition":
+                $role_color = "#08b122"; // green
+                break;
+            case "industrial":
+                $role_color = "#f8d142"; // yellow
+                break;
+            case "multi":
+                $role_color = "#3aebc8"; // cyan
+                break;
+            case "ground":
+                $role_color = "#a96f4c"; // brown
+                break;
+            case "support":
+                $role_color = "#f2aae4"; // pink
+                break;
+            case "transport":
+                $role_color = "#f68948"; // orange
+                break;
+            default:
+                $role_color = "";
+        }
+    
+        return $role_color;
     }
 }
