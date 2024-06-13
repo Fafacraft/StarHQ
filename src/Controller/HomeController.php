eecb90444736e2c1f646d24aa67a83d6187453fa
FabienArthurYnov <fabien.arthur@ynov.com>
2024-06-13 13:47:49 +0200

    didn't fix completly before, should be temporary fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -64,7 +64,7 @@ class HomeController extends AbstractController
             $ship_clear['priority'] = $priority;
 
             // change the color of the "role" part on the ship card, depending of each role
-            $ship_clear['role_color'] = getRoleColor($ship_clear['role']);
+            $ship_clear['role_color'] = $this->getRoleColor($ship_clear['role']);
             $ship_clear['with_button'] = true;
 
             array_push($ships, $ship_clear);
@@ -80,5 +80,41 @@ class HomeController extends AbstractController
 
 
         return $ships;
+    }
+
+
+    private function getRoleColor($role) {
+
+        $role_color = '';
+        switch ($role) {
+            case "combat":
+                $role_color = "#e23535"; // red
+                break;
+            case "exploration":
+                $role_color = "#2d89e1"; // blue
+                break;
+            case "competition":
+                $role_color = "#08b122"; // green
+                break;
+            case "industrial":
+                $role_color = "#f8d142"; // yellow
+                break;
+            case "multi":
+                $role_color = "#3aebc8"; // cyan
+                break;
+            case "ground":
+                $role_color = "#a96f4c"; // brown
+                break;
+            case "support":
+                $role_color = "#f2aae4"; // pink
+                break;
+            case "transport":
+                $role_color = "#f68948"; // orange
+                break;
+            default:
+                $role_color = "";
+        }
+    
+        return $role_color;
     }
 }
